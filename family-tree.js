b87f9831dfd341d8f85b7a6903d42911dd1be6f8
arjanmitra <arjanmitra@hotmail.com>
2021-01-14 21:38:52 -0500

    started family tree solution

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,23 @@
 class FamilyTree {
-  constructor () {}
+  constructor(ancestor) {
+    this.name = ancestor;
+    this.children = [];
+  }
+  insert(child) {
+    const descendant = new FamilyTree(child);
+    this.children.push(descendant);
+  }
+  findMember(name) {
+    if (this.name == name) {
+      return this;
+    } else {
+      for (let i = 0; i < this.children.length; i++) {
+        if (this.children[i].findMember(name) != undefined) {
+          return this.children[i].findMember(name);
+        }
+      }
+    }
+  }
 }
 
 module.exports = FamilyTree;
